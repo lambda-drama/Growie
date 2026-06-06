a7c3e8ee3b5619be01320a46c34c1eb38e20b91d
maniamartial <martialmania19@gmail.com>
2026-06-06 23:02:39 +0300

    feat: Have aformulae to calculate the rates

--- growie_app/investment_app/doctype/growe_holding/growe_holding.py
+++ growie_app/investment_app/doctype/growe_holding/growe_holding.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2026, Mania and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class GroweHolding(Document):
-	pass
+	def before_save(self):
+		self._sync_quantity_fields()
+		self._sync_initial_investment_value()
+
+	def _sync_quantity_fields(self) -> None:
+		"""Keep quantity and share_breakdown aligned (template + API use either)."""
+		qty = flt(self.quantity) or flt(self.share_breakdown)
+		if qty <= 0:
+			return
+		if not flt(self.quantity):
+			self.quantity = qty
+		if not flt(self.share_breakdown):
+			self.share_breakdown = qty
+
+	def _sync_initial_investment_value(self) -> None:
+		"""Initial investment = buying price × shares (in holding currency)."""
+		qty = flt(self.quantity) or flt(self.share_breakdown)
+		buy = flt(self.buying_price)
+		if qty <= 0 or buy <= 0:
+			return
+		self.initial_investment_value = buy * qty
